03973b2bc96d25ccb983d579804c04c35d4721f8
Purbita Biswas <purbitabiswas0608@gmail.com>
2021-09-16 13:29:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from reader import read_xls_csv
 from headers import UserAgents
 from scraper import Amazon
-import json
+import time
 
 from writer import GoogleSheet
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 def work():
@@ -19,9 +14,7 @@ def work():
     "sku": row[0],
     "asin": row[1],
     "product_name": row[2],
-    "keyword1": row[3],
-    "keyword2": row[4],
-    "keyword3": row[5],
+    "keywords": [row[3], row[4], row[5]]
     '''
     google_sheet = GoogleSheet()
     amzn_products = []
@@ -29,11 +22,14 @@ def work():
         ua_header = {
             'User-Agent': UA.getRandomUA()
         }
+        keywords = [product["keyword1"], product["keyword2"], product["keyword3"]]
         # print(ua_header)
-        product_details = Amazon.scrape(asin=product["asin"], product_name=product["product_name"],
-                                        keyword=product["keyword1"] or product["keyword2"] or product["keyword3"],
-                                        headers=ua_header)
-        amzn_products.append(product_details)
+        for keyword in keywords:
+            product_details = Amazon.scrape(asin=product["asin"], product_name=product["product_name"],
+                                            keyword=keyword,
+                                            headers=ua_header
+                                            )
+            amzn_products.append(product_details)
 
     print(amzn_products)
     for amzn_product in amzn_products:
